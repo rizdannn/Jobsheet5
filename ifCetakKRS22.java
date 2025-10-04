f32e9d015d2db2624baabdac4a61b1db0bc2d4f3
rizdannn <rafifprastana@gmail.com>
2025-10-04 15:39:33 +0700

    Modifikasi Percoban 1

--- ifCetakKRS22.java
+++ ifCetakKRS22.java
@@ -1,5 +1,5 @@
     import java.util.Scanner;
-    
+
     public class ifCetakKRS22 {
     public static void main(String[] args) {
 
@@ -12,6 +12,8 @@
        if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+       } else {
+        System.out.println("Register ditolak, silahkan lunasi UKT terlibih dahulu");
        }
 
 
