0013b869461be4b0fc045a004fcfa2d1f0d82e9b
rizdannn <rafifprastana@gmail.com>
2025-10-05 18:27:00 +0700

    Percobaan 3

---
+++ sistemperpustakaan22.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class sistemperpustakaan22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukka keterangan dibuku absensi");
+        System.out.println("Apakah anda membawa KTM (True/false)");
+        boolean MembawKtm = input.nextBoolean();
+    
+
+        boolean diberiAkses = MembawKtm;
+
+        System.out.println(" -- Cek Syarat Masuk Perpustakaan");
+        System.out.println("Status Kartu Mahasiswa: " + (MembawKtm ? "Ada" : " Tizdak Ada"));
+
+        if (diberiAkses) {
+            System.out.println("Keputusan: ANDA DIIZINKAN MASUKPERPUSTAKAAN");
+            System.out.println("selamat datang!");
+        } else {
+            System.out.println("Keputusan: ANDA TIDAK DIIIZINKAN MASUK PERPUSTAKAAN");
+            System.out.println("Mohon lengkapi syarat (Kartu mahasiswa atau kartu mahasiswa berbentuk dokumen)");
+        }
+
+        
+    }
+}
